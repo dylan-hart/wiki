f6e5e96257f8876e28d6ea9ecd6e0877472bd2d4
Dylan Hart <dylanhart@fastmail.com>
2026-08-22 11:53:54 -0400

    chore: delete dead pug/GraphQL admin pages, drop pug dependency
    AdminPages.vue, AdminPagesEdit.vue, AdminTags.vue and
    AdminPagesVisualize.vue are unreachable 2.5-era leftovers: unrouted,
    unimported, the last Apollo/GraphQL call-sites and the last pug
    templates in the repo, importing .gql files that don't even exist on
    disk. Delete all four plus AdminLayout's disabled experimental link to
    the unrouted tags path, and drop the now-unused pug devDependency.
    
    (OpenProject #969)

--- frontend/src/layouts/AdminLayout.vue
+++ frontend/src/layouts/AdminLayout.vue
@@ -241,16 +241,6 @@
               </w-item-section>
             </w-item>
             <w-item
-              :to="`/_admin/` + adminStore.currentSiteId + `/tags`"
-              active-class="bg-primary text-white"
-              disabled
-              v-if="flagsStore.experimental && userStore.can(`manage:sites`)">
-              <w-item-section avatar>
-                <w-icon name="img:/_assets/icons/fluent-tag.svg" />
-              </w-item-section>
-              <w-item-section>{{ t('admin.tags.title') }}</w-item-section>
-            </w-item>
-            <w-item
               :to="`/_admin/` + adminStore.currentSiteId + `/theme`"
               active-class="bg-primary text-white"
               v-if="maySeeTheme">
