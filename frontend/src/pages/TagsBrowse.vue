0ac5689e316fefe5d1dd81784ceed3da80cdbde9
Dylan Hart <dylanhart@fastmail.com>
2026-09-23 06:44:51 -0400

    Revert "Fix frontend gate: HeaderNav flush-button count and TagsBrowse no-caps"
    This reverts commit 681b55c946d62eb350f416c773fe8f61842cfbef.

--- frontend/src/pages/TagsBrowse.vue
+++ frontend/src/pages/TagsBrowse.vue
@@ -187,6 +187,7 @@
             class="tags-browse-tasks-link"
             flat
             dense
+            no-caps
             icon="tabler:checklist"
             :label="t('tasks.openTasks')"
             to="/_tasks" />
